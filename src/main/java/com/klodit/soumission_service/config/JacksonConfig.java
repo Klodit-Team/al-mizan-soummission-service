345f2c81a84a111b72a6e8d4c74584a36de7ed2b
SariYanouche <sariyanouche@gmail.com>
2026-04-26 12:02:36 +0100

    jf

--- src/main/java/com/klodit/soumission_service/config/JacksonConfig.java
+++ src/main/java/com/klodit/soumission_service/config/JacksonConfig.java
@@ -1,5 +1,6 @@
 package com.klodit.soumission_service.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature; // <-- Ajouter cet import
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -19,6 +20,7 @@ public class JacksonConfig {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES); 
         return mapper;
     }
 }
